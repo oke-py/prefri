6971381225e8a37e46e5eba96b369bf1236bbe06
Naoki Oketani <okepy.naoki@gmail.com>
2019-05-09 15:34:57 +0900

    Debug

--- main.go
+++ main.go
@@ -16,15 +16,15 @@ import (
 func prefri(ar v1beta1.AdmissionReview) *v1beta1.AdmissionResponse {
 	klog.V(2).Info("calling prefri")
 
-	if expect, actual := "deployments", ar.Request.Resource.Resource; expect != actual {
-		err := fmt.Errorf("unexpected resource: expect %s, actual %s", expect, actual)
-		klog.Error(err)
-		return toAdmissionResponse(true, nil)
-	}
+	// if expect, actual := "deployments", ar.Request.Resource.Resource; expect != actual {
+	// 	err := fmt.Errorf("unexpected resource: expect %s, actual %s", expect, actual)
+	// 	klog.Error(err)
+	// 	return toAdmissionResponse(true, nil)
+	// }
 
-	now := time.Now()
+	weekday := time.Now().Weekday()
 
-	if now.Weekday() == time.Friday {
+	if weekday == time.Thursday {
 		err := fmt.Errorf("Operation is prohibited")
 		return toAdmissionResponse(false, err)
 	}
